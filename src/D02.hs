d3c8b9c83e76ced6df694003fe1a455f1f4b1ad7
Tobin Yehle <tobinyehle@gmail.com>
2016-12-02 09:18:52 -0700

    Day 2 part 2 done

--- src/D02.hs
+++ src/D02.hs
@@ -1,13 +1,17 @@
 module D02 where
 
+import Data.Char (toUpper)
+import Numeric (showHex)
+
 data Command = U | D | L | R deriving (Show)
 
 run :: IO ()
 run = do
   commands <- fmap parseCommands . readFile $ "resources/d02.txt"
-  putStr "Command line lengths: "
-  print . map length $ commands
-  print . map getKey . execCommands (1,1) $ commands
+  putStr "Square code: "
+  putStrLn . concatMap (show . getKey squareKeypad) . execCommands squareBounds (1,1) $ commands
+  putStr "Diamond code: "
+  putStrLn . map toUpper . foldr (showHex . getKey diamondKeypad) "" . execCommands diamondBounds (2,0) $ commands
 
 parseCommands :: String -> [[Command]]
 parseCommands = map (map fromChar) . lines
@@ -18,24 +22,39 @@ parseCommands = map (map fromChar) . lines
     fromChar 'R' = R
     fromChar bad = error $ "can't parse char" ++ show bad
 
-keypad :: [[Integer]]
-keypad = [[1,2,3], [4,5,6], [7,8,9]]
+squareKeypad :: [[Integer]]
+squareKeypad = [[1,2,3], [4,5,6], [7,8,9]]
 
-getKey :: (Int, Int) -> Integer
-getKey (row, col) = (keypad !! row) !! col
+squareBounds :: (Int, Int) -> Bool
+squareBounds (row, col) = row >= 0 && row < 3 && col >= 0 && col < 3
 
-execCommand :: (Int, Int) -> Command -> (Int, Int)
-execCommand pos@(row, col) cmd = if inBounds newPos then newPos else pos
+diamondKeypad :: [[Integer]]
+diamondKeypad = [ [0,   0,   1,   0,   0]
+                , [0,   2,   3,   4,   0]
+                , [5,   6,   7,   8,   9]
+                , [0, 0xA, 0xB, 0xC,   0]
+                , [0,   0, 0xD,   0,   0]
+                ]
+
+diamondBounds :: (Int, Int) -> Bool
+diamondBounds  = (< 3) . distance
   where
-    inBounds (r,c) = r >= 0 && r < 3 && c >= 0 && c < 3
+    distance (r,c) = abs (r-2) + abs (c-2)
+
+getKey :: [[a]] -> (Int, Int) -> a
+getKey pad (row, col) = (pad !! row) !! col
+
+execCommand :: ((Int,Int) -> Bool) -> (Int, Int) -> Command -> (Int, Int)
+execCommand inBounds pos@(row, col) cmd = if inBounds newPos then newPos else pos
+  where
     move (r,c) U = (r-1,c)
     move (r,c) D = (r+1,c)
     move (r,c) L = (r,c-1)
     move (r,c) R = (r,c+1)
     newPos = move pos cmd
 
-execCommands :: (Int, Int) -> [[Command]] -> [(Int, Int)]
-execCommands _ [] = []
-execCommands start@(row, col) (cmds:rest) = rowEnd : execCommands rowEnd rest
+execCommands :: ((Int,Int) -> Bool) -> (Int, Int) -> [[Command]] -> [(Int, Int)]
+execCommands _ _ [] = []
+execCommands inBounds start@(row, col) (cmds:rest) = rowEnd : execCommands inBounds rowEnd rest
   where
-    rowEnd = foldl execCommand start cmds
+    rowEnd = foldl (execCommand inBounds) start cmds
