313a40c0da45d030ef80787bb7069bff8a0e7780
Tobin Yehle <tobinyehle@gmail.com>
2016-12-05 18:19:51 -0700

    day 4 part 2 done

--- src/D04.hs
+++ src/D04.hs
@@ -4,15 +4,19 @@ import Text.Parsec
 import Text.Parsec.Char (letter)
 import Data.List (group, sort, sortOn)
 import Data.Maybe (catMaybes)
+import Data.Char (ord, chr)
 
-type Room = ([String], Integer)
+type Room = ([String], Int)
 
 run :: IO ()
 run = do
   input <- readFile "resources/d04.txt"
-  let rooms = either (error . show) id $ parseAllRooms input
+  let validRooms = catMaybes . either (error . show) id $ parseAllRooms input
   putStr "Sum of valid sectors: "
-  print . sum . map snd . catMaybes $ rooms
+  print . sum . map snd $ validRooms
+  putStr "northpole entry: "
+  print . filter (\(name, _) -> elem "northpole" name) . map decrypt $ validRooms
+
 
 parseAllRooms :: String -> Either ParseError [Maybe Room]
 parseAllRooms = parse (endBy parseRoom newline) "d04.txt"
@@ -35,3 +39,12 @@ isValid name checksum = checksum == expectedChecksum
   where
     frequencies = map (\cs -> (length cs, cs)) . group . sort . concat $ name
     expectedChecksum = take 5 . map (head . snd) . sortOn (\(n, cs) -> (-n, cs)) $ frequencies
+
+decrypt :: Room -> Room
+decrypt (name, sector) = (map (map (cycleChar sector)) name, sector)
+
+cycleChar :: Int -> Char -> Char
+cycleChar n = toChar . (+n) . toInt
+  where
+    toInt = subtract 97 . ord
+    toChar i = chr $ (i `mod` 26) + 97
