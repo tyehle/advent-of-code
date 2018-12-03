1c5badb848f89e3b2c400d3d30276146afb420f1
Tobin Yehle <tobinyehle@gmail.com>
2018-12-03 16:45:54 -0700

    first star of day 1

--- src/Y2018/D01.hs
+++ src/Y2018/D01.hs
@@ -1,5 +1,13 @@
 module Y2018.D01 where
 
 run :: IO ()
+run = do
+  fileName <- getLine
+  file <- readFile fileName
+  print $ collectJuicyStar file
 
-run = putStrLn "hi"
+
+collectJuicyStar :: String -> Int
+collectJuicyStar file = sum $ map (read . dropPlus) . lines $ file
+  where
+    dropPlus xs = if '+' `elem` xs then tail xs else xs
