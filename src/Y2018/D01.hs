dd86f82e884ad12814d89e38ecb862b38d442960
nbertagnolli <nmbertagnolli@gmail.com>
2018-12-03 17:19:43 -0700

    Solved the juicy star and the prickley star

--- src/Y2018/D01.hs
+++ src/Y2018/D01.hs
@@ -1,13 +1,26 @@
 module Y2018.D01 where
 
+import qualified Data.Set as Set
+
 run :: IO ()
 run = do
   fileName <- getLine
   file <- readFile fileName
-  print $ collectJuicyStar file
+  let ints = parseInputList file
+  print $ collectJuicyStar ints
+  print $ collectPrickleyStar ints
 
-
-collectJuicyStar :: String -> Int
-collectJuicyStar file = sum $ map (read . dropPlus) . lines $ file
+parseInputList :: String -> [Int]
+parseInputList = map (read . dropPlus) . lines
   where
     dropPlus xs = if '+' `elem` xs then tail xs else xs
+
+collectJuicyStar :: [Int] -> Int
+collectJuicyStar = sum
+
+collectPrickleyStar :: [Int] -> Int
+collectPrickleyStar = recurseList 0 Set.empty . cycle
+  where recurseList :: Int -> Set.Set Int -> [Int] -> Int
+        recurseList currVal set xs
+            | currVal `Set.member` set = currVal
+            | otherwise = recurseList (currVal + head xs) (Set.insert currVal set) (tail xs)
