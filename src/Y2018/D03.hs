2137171d71b2652e563b97db48d68cf8a703f0e7
Tobin Yehle <tobinyehle@gmail.com>
2018-12-03 23:03:56 -0700

    c3-2

--- src/Y2018/D03.hs
+++ src/Y2018/D03.hs
@@ -3,39 +3,54 @@ module Y2018.D03 where
 import Data.List.Split
 import Data.List
 
-data Record = Record {left :: Int, top :: Int, width :: Int, height :: Int} deriving Show
+data Record = Record {num :: Int, left :: Int, top :: Int, width :: Int, height :: Int} deriving (Show, Eq)
 
 run :: IO ()
 run = do
   fileName <- getLine
-
   -- parse the input into a record
   records <- map parse . lines <$> readFile fileName
-
-  -- build association List gridpoints to is populated
-  let vals = concatMap associationList records
-
-  -- merge all maps with count
-  print $ merge vals
+  print $ roughStar records
+  print $ smoothStar records
 
 parse :: String -> Record
-parse xs = Record (read left') (read $ init top') width' height'
+parse xs = Record (read $ tail hash) (read left') (read $ init top') width' height'
   where
-      [_, _, position, size] = words xs
+      [hash, _, position, size] = words xs
       [left', top'] = splitOn "," position
       [width', height'] = map read $ splitOn "x" size
 
 associationList :: Record -> [(Int, Int)]
 -- [x y | x <- [top..top + width], y <- [left..left + height]]
-associationList (Record left' top' width' height') = do
+associationList (Record _ left' top' width' height') = do
     x <- [top'..top' + height' - 1]
     y <- [left'..left' + width' - 1]
     return (x, y)
 
 -- merge all maps with count
-merge :: [(Int, Int)] -> Int
-merge xs = length filtered
-    where
-        grouped = groupBy (\(x1, y1) (x2, y2) -> (x1 == x2) && (y1 == y2)) $ sort xs
-        lengths = map length grouped
-        filtered = filter (> 1) lengths
+merge :: [(Int, Int)] -> [Int]
+merge xs = map length grouped
+  where
+    grouped = groupBy (\(x1, y1) (x2, y2) -> (x1 == x2) && (y1 == y2)) $ sort xs
+
+
+roughStar :: [Record] -> Int
+roughStar records = length $ filter (> 1) $ merge points
+  where
+    points = concatMap associationList records
+
+
+overlaps :: Record -> Record -> Bool
+overlaps a b = any (> 1) $ merge (as ++ bs)
+  where
+    as = associationList a
+    bs = associationList b
+
+
+smoothStar :: [Record] -> Maybe Record
+smoothStar records = findGood records
+  where
+    findGood [] = Nothing
+    findGood (r:rs)
+      | all (not . overlaps r) (filter (/= r) records) = Just r
+      | otherwise = findGood rs
