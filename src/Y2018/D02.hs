280769d0540fc952d4e0c1c98032b1021020ec9c
Annie Cherkaev <annie.cherk@gmail.com>
2018-12-03 20:50:54 -0700

    d2 part 2

--- src/Y2018/D02.hs
+++ src/Y2018/D02.hs
@@ -2,6 +2,7 @@ module Y2018.D02 where
 
 import qualified Data.Map as Map
 import Data.List
+import Data.Maybe (catMaybes)
 
 
 
@@ -11,16 +12,28 @@ run = do
   fileName <- getLine
   ids <- lines <$> readFile fileName
   print $ checksum ids
+  print $ spicyStar ids
 
 
 checksum :: [String] -> Int
-checksum ids = (count . map fst $ freaks) * (count . map snd $ freaks)
+checksum ids = (count id . map fst $ freaks) * (count id . map snd $ freaks)
   where freaks = map frequencies ids
 
--- thanks SO
-count list = sum $ map fromEnum list
+count :: (a -> Bool) -> [a] -> Int
+count p = length . filter p
 
 
 frequencies :: Ord a => [a] -> (Bool, Bool)
 frequencies input = (2 `elem` counts, 3 `elem` counts)
   where counts = map length . group . sort $ input
+
+spicyStar :: [String] -> Maybe String
+spicyStar xs = getMatching <$> goodPairHuh
+  where
+    getMatching pair = catMaybes $ uncurry (zipWith (\a b -> if a == b then Just a else Nothing)) pair
+    goodPairHuh :: Maybe (String, String)
+    goodPairHuh = find ((== 1) . uncurry distance) pairs
+    pairs :: [(String, String)]
+    pairs = [(a, b) | a <- xs, b <- xs]
+    distance :: String -> String -> Int
+    distance a b = count id $ zipWith (/=) a b
