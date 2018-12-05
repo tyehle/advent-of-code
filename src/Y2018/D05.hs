58b4cc19ed5fc2f44e94b5641b5c4636cee155c2
nbertagnolli <nmbertagnolli@gmail.com>
2018-12-05 13:58:00 -0700

    DAY FIVE IS IN DA BAG

--- src/Y2018/D05.hs
+++ src/Y2018/D05.hs
@@ -1,6 +1,7 @@
 module Y2018.D05 where
 
 import Data.Char
+import qualified Data.Set as Set
 
 
 
@@ -17,6 +18,7 @@ run = do
   polymer <- init <$> readFile fileName
   let answer = reduce polymer
   print . length $ answer
+  print $ stickyStar answer
 
 xor :: Bool -> Bool -> Bool
 xor False True = True
@@ -31,4 +33,13 @@ reduce x = let next = reduceRec "" x in if x == next then x else reduce next
         reduceRec done (x1:x2:xs)
             | (isUpper x1 `xor` isUpper x2) && (toLower x1 == toLower x2) = reduceRec done xs
             | otherwise = reduceRec (x1:done)  (x2:xs)
-        reduceRec done x = reverse done ++ x
+        reduceRec done remaining = reverse done ++ remaining
+
+deletePair :: Char -> String -> String
+deletePair c = filter ((/= toLower c) . toLower)
+
+stickyStar :: String -> Int
+stickyStar polymer = minimum possibilities
+    where
+        alphabet = Set.fromList . map toLower $ polymer
+        possibilities = map (\c -> length $ reduce $ deletePair c polymer) $ Set.toList alphabet
