2ca8513ec8b10ceb5f54941a2ca54950f6fa13f6
Tobin Yehle <tobinyehle@gmail.com>
2018-12-05 16:27:58 -0700

    improved skeleton for d6!

--- src/Y2018/D06.hs
+++ src/Y2018/D06.hs
@@ -1,6 +1,16 @@
 module Y2018.D06 where
 
+
 run :: String -> IO ()
 run fileName = do
   _ <- readFile fileName
-  putStrLn "Testing :)"
+  putStrLn $ saffronStar ""
+  putStrLn $ sumacStar ""
+
+
+saffronStar :: String -> String
+saffronStar _ = "Saffron!"
+
+
+sumacStar :: String -> String
+sumacStar _ = "Sumac!"
