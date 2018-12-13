586ee42d77895f8a82a7eedc121239af40a4d710
Tobin Yehle <tobinyehle@gmail.com>
2018-12-13 15:34:28 -0700

    day 11 part two done

--- src/Y2018/D11.hs
+++ src/Y2018/D11.hs
@@ -1,8 +1,9 @@
 module Y2018.D11 where
 
 
-import Data.Map (Map)
-import qualified Data.Map as Map
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Control.Monad.State.Strict
 
 
 type Loc = (Int, Int)
@@ -14,23 +15,50 @@ run input = do
       sn = read input
       powerLevels = mkPowerLevels sn
   print $ squareStar powerLevels 3
-  print $ cubicalStar powerLevels
+  print $ evalState (maxPower (powerLevels Map.!)) Map.empty
 
 
-maxSubSeq :: (Num a, Ord a) => [a] -> [a]
-maxSubSeq [] = []
-maxSubSeq (first:rest) = reverse $ go (first, [first]) (first, [first]) rest
+maxPower :: (Loc -> Int) -> State (Map (Loc, Int) Int) (Int, (Loc, Int))
+maxPower value = do
+  let ((loc, n):rest) = allBoxes
+  p <- powerOf value loc n
+  go (loc, n) p rest
   where
-    go :: (Num a, Ord a) => (a, [a]) -> (a, [a]) -> [a] -> [a]
-    go (_, best) _ [] = best
-    go best (scoreHere, here) (x:xs) = go nextBest nextHere xs
-      where
-        nextHere = if x > scoreHere + x
-                   then (x, [x])
-                   else (x + scoreHere, x:here)
-        nextBest = if fst nextHere > fst best
-                   then nextHere
-                   else best
+    allBoxes :: [(Loc, Int)]
+    allBoxes = [((x, y), n)
+               | n <- [1..300]
+               , x <- [1..300-n+1]
+               , y <- [1..300-n+1]
+               ]
+    go :: (Loc, Int) -> Int -> [(Loc, Int)] -> State (Map (Loc, Int) Int) (Int, (Loc, Int))
+    go best bestPower [] = return (bestPower, best)
+    go best bestPower ((loc, n):xs) = do
+      power <- powerOf value loc n
+      if power > bestPower
+        then go (loc, n) power xs
+        else go best bestPower xs
+
+
+powerOf :: (Loc -> Int) -> Loc -> Int -> State (Map (Loc, Int) Int) Int
+powerOf _ _ 0 = return 0
+powerOf value loc 1 = return $ value loc
+powerOf value (x, y) n = do
+  maybeAnswer <- gets $ Map.lookup ((x, y), n)
+  case maybeAnswer of
+    Just answer -> return answer
+    Nothing -> do
+      answer <- compute
+      modify' $ Map.insert ((x, y), n) answer
+      return answer
+  where
+    compute :: State (Map (Loc, Int) Int) Int
+    compute = do
+      topRight <- powerOf value (x, y) (n-1)
+      bottomLeft <- powerOf value (x+1, y+1) (n-1)
+      center <- powerOf value (x+1, y+1) (n-2)
+      let topLeft = value (x+n-1, y)
+          bottomRight = value (x, y+n-1)
+      return (topRight + topLeft + bottomRight + bottomLeft - center)
 
 
 mkPowerLevels :: Int -> Map Loc Int
@@ -48,10 +76,6 @@ powerLevel sn (x, y) = power4 - 5
     power2 = power1 + sn
     power3 = power2 * rackID
     power4 = (power3 `div` 100) `mod` 10
-
-
-cubicalStar :: Map Loc Int -> ((Int, Loc), Int)
-cubicalStar powerLevels = maximum [(squareStar powerLevels size, size) | size <- [1..300]]
 
 
 squareStar :: Map Loc Int -> Int -> (Int, Loc)
