d72fb8a4e84453283f41d69212244751339b2196
Tobin Yehle <tobinyehle@gmail.com>
2018-12-20 12:33:38 -0700

    day 16 done

--- src/ParseUtil.hs
+++ src/ParseUtil.hs
@@ -1,5 +1,6 @@
 module ParseUtil
   ( unsafeParse
+  , nat
   ) where
 
 
@@ -8,3 +9,7 @@ import Text.Parsec
 
 unsafeParse :: Parsec String () a -> SourceName -> String -> a
 unsafeParse parser file = either (error . show) id . parse parser file
+
+
+nat :: Parsec String a Int
+nat = read <$> many1 digit
