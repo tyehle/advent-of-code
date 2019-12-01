23a0c5db77b635f9275f91e31b5242b10eb3b57f
Tobin Yehle <tobinyehle@gmail.com>
2019-12-01 13:53:08 -0800

    finish day 1

--- 2019/d01/src/main.rs
+++ 2019/d01/src/main.rs
@@ -1,3 +1,41 @@
+use std::fs;
+
+fn fuel_for(mass: i32) -> i32 {
+    mass / 3 - 2
+}
+
+fn module_fuel(mass: i32) -> i32 {
+    let mut total = 0;
+    let mut current = fuel_for(mass);
+    while current > 0 {
+        total += current;
+        current = fuel_for(current);
+    }
+    total
+}
+
+fn parse_input(path: &str) -> Vec<i32> {
+    fs::read_to_string(path)
+        .unwrap()
+        .lines()
+        .map(|l| l.parse().unwrap())
+        .collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = parse_input("input.txt");
+    println!("{}", input.iter().map(|m| fuel_for(*m)).sum::<i32>());
+    println!("{}", input.iter().map(|m| module_fuel(*m)).sum::<i32>());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_module_fuel() {
+        assert_eq!(2, module_fuel(14));
+        assert_eq!(966, module_fuel(1969));
+        assert_eq!(50346, module_fuel(100756));
+    }
 }
