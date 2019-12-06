ec00a8d53966f706b41909df950451337770e22e
Tobin Yehle <tyehle@mmm.com>
2019-12-05 16:38:41 -0800

    improve day 4 performance

--- 2019/d04/src/main.rs
+++ 2019/d04/src/main.rs
@@ -1,35 +1,33 @@
-
-
-fn is_valid(pw: Vec<i32>) -> bool {
+fn is_valid(pw: &[u8]) -> bool {
     let mut double = false;
     let mut prev = pw[0];
 
-    for i in 1..pw.len() {
-        if pw[i] < prev {
+    for d in pw.iter().skip(1) {
+        if *d < prev {
             return false;
         }
 
-        if pw[i] == prev {
+        if *d == prev {
             double = true;
         }
 
-        prev = pw[i];
+        prev = *d;
     }
 
     double
 }
 
-fn is_valid_b(pw: Vec<i32>) -> bool {
+fn is_valid_b(pw: &[u8]) -> bool {
     let mut run = 0;
     let mut double = false;
     let mut prev = pw[0];
 
-    for i in 1..pw.len() {
-        if pw[i] < prev {
+    for d in pw.iter().skip(1) {
+        if *d < prev {
             return false;
         }
 
-        if pw[i] == prev {
+        if *d == prev {
             run += 1;
         } else {
             if run == 1 {
@@ -38,43 +36,64 @@ fn is_valid_b(pw: Vec<i32>) -> bool {
             run = 0;
         }
 
-        prev = pw[i];
+        prev = *d;
     }
 
     double || run == 1
 }
 
-fn digits(n: i32) -> Vec<i32> {
-    n.to_string().chars().map(|c| c.to_string().parse().unwrap()).collect()
+fn digits(n: i32) -> Vec<u8> {
+    n.to_string()
+        .chars()
+        .map(|c| c.to_string().parse().unwrap())
+        .collect()
 }
 
-fn brute_count_a(low: i32, high: i32) -> i32 {
-    let mut count = 0;
+fn inc(ds: &mut Vec<u8>) {
+    let mut pos = ds.len() - 1;
 
-    for i in low..high {
-        if is_valid(digits(i)) {
-            count += 1;
-        }
+    // look for the first digit that's not a 9
+    while ds[pos] >= 9 {
+        pos -= 1;
     }
 
-    count
+    // set digit
+    ds[pos] += 1;
+
+    // set digits below
+    for i in pos + 1..ds.len() {
+        ds[i] = ds[i - 1];
+    }
 }
 
-fn brute_count_b(low: i32, high: i32) -> i32 {
+fn counter(low: i32, high: i32, predicate: impl Fn(&[u8]) -> bool) -> u32 {
+    let mut ds = digits(low);
+    let end = digits(high);
     let mut count = 0;
 
-    for i in low..high {
-        if is_valid_b(digits(i)) {
+    loop {
+        if predicate(&ds) {
             count += 1;
         }
-    }
 
-    count
+        inc(&mut ds);
+
+        // check end condition
+        for i in 0..ds.len() {
+            if end[i] > ds[i] {
+                break;
+            }
+
+            if end[i] < ds[i] {
+                return count;
+            }
+        }
+    }
 }
 
 fn main() {
-    println!("{}", brute_count_a(153517, 630395));
-    println!("{}", brute_count_b(153517, 630395));
+    println!("{}", counter(153_517, 630_395, is_valid));
+    println!("{}", counter(153_517, 630_395, is_valid_b));
 }
 
 #[cfg(test)]
@@ -83,7 +102,14 @@ mod test {
 
     #[test]
     fn test_b_valid() {
-        assert!(is_valid_b(digits(111122)));
-        assert!(!is_valid_b(digits(123444)));
+        assert!(is_valid_b(&digits(111_122)));
+        assert!(!is_valid_b(&digits(123_444)));
+    }
+
+    #[test]
+    fn test_inc() {
+        let mut ds = vec![3, 5, 5, 9, 9];
+        inc(&mut ds);
+        assert_eq!(ds, vec![3, 5, 6, 6, 6]);
     }
 }
