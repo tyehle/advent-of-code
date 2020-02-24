90dae1795538cc2aecdc26e94ae51291468a3ff8
Tobin Yehle <tobinyehle@gmail.com>
2020-02-24 17:56:12 -0500

    finish day 14

--- 2019/d14/src/main.rs
+++ 2019/d14/src/main.rs
@@ -2,15 +2,15 @@ use std::collections::HashMap;
 use std::fs;
 
 
-fn parse_input() -> HashMap<String, (i32, Vec<(i32, String)>)> {
+fn parse_input() -> HashMap<String, (i64, Vec<(i64, String)>)> {
     parse(&fs::read_to_string("input.txt").unwrap())
 }
 
-fn parse(input: &str) -> HashMap<String, (i32, Vec<(i32, String)>)> {
+fn parse(input: &str) -> HashMap<String, (i64, Vec<(i64, String)>)> {
     input.trim().lines().map(parse_reaction).collect()
 }
 
-fn parse_reaction(line: &str) -> (String, (i32, Vec<(i32, String)>)) {
+fn parse_reaction(line: &str) -> (String, (i64, Vec<(i64, String)>)) {
     match line.split(" => ").collect::<Vec<_>>()[..] {
         [ins, output] => {
             let (amount, name) = parse_chemical(output);
@@ -23,34 +23,84 @@ fn parse_reaction(line: &str) -> (String, (i32, Vec<(i32, String)>)) {
     }
 }
 
-fn parse_chemical(raw: &str) -> (i32, String) {
+fn parse_chemical(raw: &str) -> (i64, String) {
     match raw.trim().split(' ').collect::<Vec<_>>()[..] {
         [num, name] => (num.parse().unwrap(), name.to_string()),
         _ => panic!(""),
     }
 }
 
-fn p1(rules: &HashMap<String, (i32, Vec<(i32, String)>)>) {
-    let mut required: HashMap<String, i32> = HashMap::new();
-    let mut extra: HashMap<String, i32> = HashMap::new();
-    required.insert("FUEL".to_string(), 1);
+fn p1(rules: &HashMap<String, (i64, Vec<(i64, String)>)>, fuel: i64) -> i64 {
+    let mut required: HashMap<&str, i64> = HashMap::new();
+    let mut extra: HashMap<&str, i64> = HashMap::new();
+    required.insert("FUEL", fuel);
 
-    fn not_done(p: (&String, &i32)) -> bool {
-        p.1 > &0 && p.0 != "ORE"
+    fn not_done(p: (&&str, &i64)) -> bool {
+        p.1 > &0 && *p.0 != "ORE"
     }
 
-    while let Some((name, required_amount)) = required.iter().find(|p| not_done(*p)) {
-        let (produced_amount, inputs) = &rules[name];
-        let times = (required_amount - 1) / produced_amount + 1;
-        let extra_amount = produced_amount * times - required_amount;
+    loop {
+        let name = match required.iter().find(|p| not_done(*p)) {
+            None => break,
+            Some((name, _)) => *name,
+        };
+
+        // Check if we have what we need in `extra`
+        let amount_in_extra = extra.get(name).unwrap_or(&0);
+        let reduced_amount = amount_in_extra - required[name];
+        if reduced_amount >= 0 {
+            // Don't run rule, take from extra
+            extra.insert(name, reduced_amount);
+        } else {
+            // Use all from extra, and run modified rule
+            let amount_needed = -reduced_amount;
+            let (produced_amount, inputs) = &rules[name];
+            let times = amount_needed / produced_amount + if amount_needed % produced_amount == 0 {0} else {1};
+            // put inputs into required
+            extra.insert(name, produced_amount * times - amount_needed);
+            // put any extra into extra
+            for (amount, name) in inputs {
+                let value = required.entry(name).or_insert(0);
+                *value += amount * times;
+            }
+        }
+        required.remove(name);
     }
 
-    unimplemented!();
+    required["ORE"]
+}
+
+fn hackx(ore: i64, rules: &HashMap<String, (i64, Vec<(i64, String)>)>) -> i64 {
+    let mut lower_bound = 1;
+    let mut upper_bound = 0;
+    let mut guess = 2;
+
+    while upper_bound == 0 {
+        if p1(rules, guess) > ore {
+            upper_bound = guess;
+            break;
+        } else {
+            lower_bound = guess;
+            guess *= 2;
+        }
+    }
+
+    while upper_bound - lower_bound > 1 {
+        guess = (lower_bound + upper_bound) / 2;
+        if p1(rules, guess) > ore {
+            upper_bound = guess;
+        } else {
+            lower_bound = guess;
+        }
+    }
+
+    lower_bound
 }
 
 fn main() {
     let input = parse_input();
-    p1(&input)
+    println!("{}", p1(&input, 1));
+    println!("{}", hackx(1_000_000_000_000, &input));
 }
 
 #[cfg(test)]
