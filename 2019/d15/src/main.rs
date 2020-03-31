f71d92442dffd723709fa0107d42c28bfa770d83
Tobin Yehle <tobinyehle@gmail.com>
2020-03-31 00:17:47 -0700

    fully explore the space

--- 2019/d15/src/main.rs
+++ 2019/d15/src/main.rs
@@ -1,12 +1,11 @@
+use console::{style, Term};
 use num_complex::Complex;
 use num_traits::identities::Zero;
 use std::collections::HashMap;
 use std::fs;
-use console::{Term, style};
 
 mod intcode;
 use intcode::*;
-
 
 #[derive(Debug)]
 enum Direction {
@@ -17,6 +16,16 @@ enum Direction {
 }
 
 impl Direction {
+    fn iter() -> std::slice::Iter<'static, Direction> {
+        [
+            Direction::North,
+            Direction::East,
+            Direction::South,
+            Direction::West,
+        ]
+        .iter()
+    }
+
     fn code(&self) -> i64 {
         match self {
             Direction::North => 1,
@@ -34,8 +43,17 @@ impl Direction {
             Direction::East => Complex::new(1, 0),
         }
     }
-}
 
+    fn from_unit(unit: Complex<i64>) -> Option<Direction> {
+        match unit {
+            Complex { re: 0, im: 1 } => Some(Direction::North),
+            Complex { re: 0, im: -1 } => Some(Direction::South),
+            Complex { re: -1, im: 0 } => Some(Direction::West),
+            Complex { re: 1, im: 0 } => Some(Direction::East),
+            _ => None,
+        }
+    }
+}
 
 enum Tile {
     Blank,
@@ -43,9 +61,9 @@ enum Tile {
     Oxygen,
 }
 
-
 struct Droid {
     controller: State,
+    o2_len: Option<usize>,
     path: Vec<Complex<i64>>,
     pos: Complex<i64>,
     map: HashMap<Complex<i64>, Tile>,
@@ -61,7 +79,7 @@ impl Droid {
                 // Hit a wall
                 self.map.insert(self.pos + direction.unit(), Tile::Wall);
                 self.pos
-            },
+            }
 
             Some(1) => {
                 // Moved
@@ -72,6 +90,11 @@ impl Droid {
 
             Some(2) => {
                 // Found the thing
+                if self.o2_len.is_none() {
+                    // Don't update this again once we know
+                    // If we step back through it things will break
+                    self.o2_len = Some(self.path.len() + 1);
+                }
                 let new_pos = self.pos + direction.unit();
                 self.map.insert(new_pos, Tile::Oxygen);
                 new_pos
@@ -93,38 +116,53 @@ impl Droid {
 
     fn auto_step(&mut self, count: u32) {
         for _ in 0..count {
-            // TODO
+            // are there places to explore around us?
+            let unexplored =
+                Direction::iter().find(|dir| !self.map.contains_key(&(self.pos + dir.unit())));
+
+            match unexplored {
+                Some(dir) => self.step(dir),
+                None => {
+                    match self.path.last() {
+                        None => return, // The space is fully epxlored
+                        Some(new_pos) => {
+                            self.step(&Direction::from_unit(new_pos - self.pos).unwrap())
+                        }
+                    }
+                }
+            }
         }
     }
 }
-
 
 fn parse() -> Vec<i64> {
     parse_program(fs::read_to_string("input.txt").unwrap())
 }
 
-
 fn init_output() {
     // Clear the screen
-    print!("\u{1B}[2J");
+    print!("\u{1B}[2J\u{1B}[?25l");
 }
 
+fn finish_output() {
+    print!("\u{1B}[?25h");
+}
 
 fn print_world(droid: &Droid) {
     let spaces = droid.map.keys().collect::<Vec<_>>();
 
-    let min_y = spaces.iter().min_by(|a, b| a.im.cmp(&b.im)).unwrap().im;
-    let max_y = spaces.iter().max_by(|a, b| a.im.cmp(&b.im)).unwrap().im + 5;
+    let min_y = spaces.iter().min_by(|a, b| a.im.cmp(&b.im)).unwrap().im - 2;
+    let max_y = spaces.iter().max_by(|a, b| a.im.cmp(&b.im)).unwrap().im + 2;
 
     let min_x = spaces.iter().min_by(|a, b| a.re.cmp(&b.re)).unwrap().re - 5;
-    let max_x = spaces.iter().max_by(|a, b| a.re.cmp(&b.re)).unwrap().re;
+    let max_x = spaces.iter().max_by(|a, b| a.re.cmp(&b.re)).unwrap().re + 5;
 
     fn tile_glyph(tile: Option<&Tile>) -> console::StyledObject<&'static str> {
         match tile {
             None => style(" "),
-            Some(Tile::Blank) => style("."),
-            Some(Tile::Wall) => style("#"),
-            Some(Tile::Oxygen) => style("X").blue(),
+            Some(Tile::Blank) => style(".").black(),
+            Some(Tile::Wall) => style("#").bold().black(),
+            Some(Tile::Oxygen) => style("O").cyan(),
         }
     }
 
@@ -134,7 +172,7 @@ fn print_world(droid: &Droid) {
             let loc = Complex::new(col, row);
             let glyph = {
                 if droid.pos == loc {
-                    style("D").red()
+                    style("D").red().bold()
                 } else {
                     tile_glyph(droid.map.get(&loc))
                 }
@@ -145,10 +183,10 @@ fn print_world(droid: &Droid) {
     }
 }
 
-
 fn interact(instructions: &[i64]) {
     let mut droid = Droid {
         controller: State::new(instructions),
+        o2_len: None,
         path: Vec::new(),
         pos: Complex::zero(),
         map: HashMap::new(),
@@ -160,21 +198,23 @@ fn interact(instructions: &[i64]) {
 
     init_output();
     loop {
-        let direction = match console.read_char().unwrap() {
-            'w' => Direction::North,
-            'a' => Direction::West,
-            's' => Direction::South,
-            'd' => Direction::East,
-            '\n' => {droid.auto_step(10); continue},
-            '\u{4}' => break, // eof
+        match console.read_char().unwrap() {
+            // 'w' => droid.step(&Direction::North),
+            // 'a' => droid.step(&Direction::West),
+            // 's' => droid.step(&Direction::South),
+            // 'd' => droid.step(&Direction::East),
+            ' ' => droid.auto_step(1),
+            'n' => droid.auto_step(10),
+            '\n' => droid.auto_step(100),
+            '\u{4}' | 'q' => break, // eof
             _ => continue,
         };
 
-        droid.step(&direction);
         print_world(&droid);
     }
+    finish_output();
 
-    println!("{}", droid.path.len());
+    println!("Path to O2: {:?}", droid.o2_len);
 }
 
 fn main() {
