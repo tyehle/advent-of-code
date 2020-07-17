c75b9d9a835d9ab21402413e5d34f05fe4b4baec
Tobin Yehle <tobinyehle@gmail.com>
2020-07-16 22:18:41 -0700

    day 17 skeleton

---
+++ 2019/d17/src/main.rs
@@ -0,0 +1,13 @@
+use std::fs;
+
+mod intcode;
+use intcode::*;
+
+fn parse() -> Vec<i64> {
+    parse_program(fs::read_to_string("input.txt").unwrap())
+}
+
+fn main() {
+    let input = parse();
+    println!("{:?}", input);
+}
